a26d5889e78a84cbba0dbe73c53d137f0850c27c
emmiechege <emilywanjachege@gmail.com>
2023-11-30 23:52:28 +0300

    a program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Handles basic arithmetic operations"""
-    from calculator_1 import add, sub, mul, div
-    import sys
+import sys
+from calculator_1 import add, sub, mul, div
 
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
 
-        ops = {"+": add, "-": sub, "*": mul, "/": div}
-        if sys.argv[2] not in list(ops.keys()):
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+
+    if __name__ == "__main__":
+        if len(sys.argv) != 4:
+            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+            exit(1)
 
         a = int(sys.argv[1])
+        operator = (sys.argv[2])
         b = int(sys.argv[3])
-        print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+
+        if operator == "+":
+            solution = add(a, b)
+        elif operator == "-":
+            solution = sub(a, b)
+        elif operator == "*":
+            solution = mul(a, b)
+        elif operator == "/":
+            solution = div(a, b)
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+        print("{} {} {} = {}".format(a, operator, b, solution))
