d123374295281499672d51d9aac4085b5c3d092a
emmiechege <emilywanjachege@gmail.com>
2023-12-04 00:48:17 +0300

    a function that adds 2 tuples.

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#1/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    e = list(tuple_a)   # convert tuple to list
+    f = list(tuple_b)
+
+    while len(e) < 2:
+        e.append(0)
+    while len(f) < 2:
+        f.append(0)
+
+    e = e[:2]   # two elements per tuple
+    f = f[:2]
+    return (e[0] + f[0], e[1] +f[1])
